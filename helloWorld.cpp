71fd1b44f542ec95e9f8fa12d751097fec314948
mattyan1726 <32181891+mattyan1726@users.noreply.github.com>
2018-01-18 14:07:41 -0600

    story

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-  cout<<"LSHDDDDDXB!!!";
-  cout<<"Hello World!";
-  cout<<"DEERY!!!";
-  cout<<"Let's chat";
+  cout<<"Background:  英语课，两个同桌睡着了”；
+  cout<<"老师：孟旭阳韦绍博起床了！（梦 续阳痿 少勃起 床 了）"
   return 0;
 }
